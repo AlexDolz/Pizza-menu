0fa8a2721d0f9447be219ecbb66df442fbda08db
AlexDolz <aleksejdolznikov3@gmail.com>
2024-08-23 12:37:23 +0200

    destructuring props

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -126,7 +126,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
@@ -136,10 +136,13 @@ function Footer() {
   );
 }
 
-function Order({ closeHour }) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
-      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
       <button className="btn">Order</button>
     </div>
   );
